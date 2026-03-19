eaa2e15dee92d26a15b2aea417bea5bf4fbdb9eb
Daniel Joshua Damilola <114642431+JoshDan2468@users.noreply.github.com>
2026-03-19 04:37:51 +0100

    Fix base path issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/globalexpert_website/", // 👈 ADD THIS LINE
+  base: mode === "production"
+    ? "/globalexpert_website/"   // for GitHub Pages
+    : "/",                       // for local + Vercel
 
   server: {
     host: "::",
